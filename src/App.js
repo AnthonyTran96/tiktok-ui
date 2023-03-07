170402c452fb8d5cf09a7ffc60440e969f3b5046
AnthonyTran <quanghust96@gmail.com>
2023-03-08 01:30:06 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
 function App() {
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={<Page />} />;
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
