7d1a064c98bb2d9c0aa264d6a2615ca00accea6f
Anthony Tran <quanghust96@gmail.com>
2023-03-08 13:06:50 +0700

    router

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+function DefaultLayput({children}) {
+    return ( <div>
+        <Header/>
+        <div className="container">
+            <Sidebar/>
+            <div className="content">{children}</div>
+        </div>
+    </div> );
+}
+
+export default DefaultLayput;
