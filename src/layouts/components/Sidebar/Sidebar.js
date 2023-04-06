42d719d07a70786a433338b5c880b41dd7491a50
Anthony Tran <quanghust96@gmail.com>
2023-04-06 14:21:18 +0700

    xay dung UI Sidebar 1

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -2,7 +2,14 @@ import styles from './Sidebar.module.scss';
 import classNames from 'classnames/bind';
 import config from '~/config';
 import Menu, { MenuItem } from './Menu';
-import { HomeIcon, UserGroupIcon, CameraIcon } from '~/components/icons';
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    UserGroupIcon,
+    UserGroupActiveIcon,
+    CameraIcon,
+    CameraActiveIcon,
+} from '~/components/icons';
 
 const cx = classNames.bind(styles);
 
@@ -10,9 +17,19 @@ function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
             <Menu>
-                <MenuItem title="For you" to={config.routes.root} icon={<HomeIcon />} />
-                <MenuItem title="Following" to={config.routes.following} icon={<UserGroupIcon />} />
-                <MenuItem title="LIVE" to={config.routes.live} icon={<CameraIcon />} />
+                <MenuItem title="For you" to={config.routes.root} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem
+                    title="LIVE"
+                    to={config.routes.live}
+                    icon={<CameraIcon />}
+                    activeIcon={<CameraActiveIcon />}
+                />
             </Menu>
         </aside>
     );
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,10 +5,12 @@ import Following from '~/pages/Following';
 import Upload from '~/pages/Upload';
 import { HeaderOnly } from '~/layouts';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 const publicRoutes = [
     { path: config.routes.root, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
 ];
